7159a0b8221a0de9c514c8c7dfdb277a12c12658
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-06-10 15:21:17 -0700

    Removed the class exists statement, not needed, and checks against the path. Oops.

--- sample_interface.php
+++ sample_interface.php
@@ -2,8 +2,8 @@
 	// working in same path so just need to provide file name
 	$config_file_path = 'sample_config.php';
 	
-	if(!class_exists($config_file_path))
-		require($config_file_path);
+	
+	require($config_file_path);
 	// make new class
 	$config = new conf_class($config_file_path);
 	// now display it
